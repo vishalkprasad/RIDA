a2e92ea12de3aa416c7928afb17274d503ddd8e4
vishalkprasad <vishalkumar.prasad5@gilead.com>
2024-12-24 16:49:34 +0530

    Update and rename helloword.java to Addition.java

---
+++ src/Addition.java
@@ -0,0 +1,9 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
