acac5a7d64d69360c389a8fa6b603b3561f7f9a6
vishalkprasad <vishalkumar.prasad5@gilead.com>
2025-01-24 00:56:33 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-cp appspec-dev.yml appspec.yml
+# Determine the environment and copy the corresponding appspec file
+if [ -f /opt/deployment/appspec-dev.yml ]; then
+    cp /opt/deployment/appspec-dev.yml /opt/deployment/appspec.yml
+elif [ -f /opt/deployment/appspec-tst.yml ]; then
+    cp /opt/deployment/appspec-tst.yml /opt/deployment/appspec.yml
+elif [ -f /opt/deployment/appspec-prd.yml ]; then
+    cp /opt/deployment/appspec-prd.yml /opt/deployment/appspec.yml
+else
+    echo "No matching appspec file found."
+    exit 1
+fi
