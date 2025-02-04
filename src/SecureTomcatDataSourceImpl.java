9c61faa4622d6e6a9c60609c3f66b5e3bb767da3
vishalkprasad <vishalkumar.prasad5@gilead.com>
2025-02-04 19:34:57 +0530

    Create SecureTomcatDataSourceImpl.java

---
+++ src/SecureTomcatDataSourceImpl.java
@@ -0,0 +1,61 @@
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.Properties;
+ 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.naming.Context;
+import javax.sql.DataSource;
+ 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.tomcat.jdbc.pool.DataSourceFactory;
+import org.apache.tomcat.jdbc.pool.PoolConfiguration;
+import org.apache.tomcat.jdbc.pool.XADataSource;
+ 
+public class SecureTomcatDataSourceImpl extends DataSourceFactory {
+ 
+    private static final Log log = LogFactory.getLog(SecureTomcatDataSourceImpl.class);
+ 
+    private GPVPasswordSDK gpv = null;
+ 
+    public SecureTomcatDataSourceImpl() {
+        try {
+            gpv = new GPVPasswordSDK();
+        } catch (Exception e) {
+            log.fatal("Error instantiating decryption class.", e);
+            throw new RuntimeException(e);
+        }
+    }
+ 
+    @Override
+    public DataSource createDataSource(Properties properties, Context context, boolean XA) throws InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException, SQLException, NoSuchAlgorithmException,
+            NoSuchPaddingException {
+        // Here we decrypt our password.
+        PoolConfiguration poolProperties = SecureTomcatDataSourceImpl.parsePoolProperties(properties);
+        poolProperties.setPassword(gpv.getPassword());
+ 
+        // The rest of the code is copied from Tomcat's DataSourceFactory.
+        if (poolProperties.getDataSourceJNDI() != null && poolProperties.getDataSource() == null) {
+            performJNDILookup(context, poolProperties);
+        }
+        org.apache.tomcat.jdbc.pool.DataSource dataSource = XA ? new XADataSource(poolProperties)
+                : new org.apache.tomcat.jdbc.pool.DataSource(poolProperties);
+       
+        String Name = SecureTomcatDataSourceImpl.getProperties("name").toString();
+        String URL = poolProperties.getUrl();
+        String username = poolProperties.getUsername();
+        Logger.getLogger(SecureTomcatDataSourceImpl.class.getName()).log(Level.INFO, "Creating a New Connection Pool for DataSource "+Name+" with URL "+URL+" and the  username "+username);
+       
+        dataSource.createPool();
+ 
+        return dataSource;
+    }
+ 
+}
